9e4c0ce55407fbf3e2d1fdd8e3453faa0e5e749d
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-06-03 19:48:34 +0100

    Update Enable-UAC.ps1

--- includes/Enable-UAC.ps1
+++ includes/Enable-UAC.ps1
@@ -1,2 +1,6 @@
-# Enable User Account Control (UAC) 
-Set-ItemProperty -Path HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA -Value 1 -Type DWord
+# Enable UAC
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA -Value 1 -Type DWord
+
+# Set User Account Control (UAC) to 'Notify only when apps try to make changes'
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
+  -Name "ConsentPromptBehaviorAdmin" -Value 5
