19a4623baee519b13595449d753263da44d9762d
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-06-03 19:45:10 +0100

    Update Configure-AccountLockout.ps1
    more comments added.

--- includes/Configure-AccountLockout.ps1
+++ includes/Configure-AccountLockout.ps1
@@ -1,4 +1,11 @@
 # Account lockout policy
+
+# Number of failed login attempts before the account is locked
 net accounts /lockoutthreshold:5
+
+# Duration (in minutes) that the account remains locked out
 net accounts /lockoutduration:30
+
+# Time window (in minutes) during which failed login attempts are counted
+# If 5 failed attempts occur within this 30-minute window, the account is locked
 net accounts /lockoutwindow:30
