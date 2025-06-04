6d958cc8d84c3d974b9c8bcd16d11293aaca14e6
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-06-04 19:30:13 +0100

    Update IE ESC script with server check and restart

--- includes/Disable-IE-Security-Admins.ps1
+++ includes/Disable-IE-Security-Admins.ps1
@@ -1,10 +1,26 @@
 # Disable IE Enhanced Security Configuration (ESC)
 function Disable-IEESC {
-    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-    Stop-Process -Name Explorer
+    $isServer = ((Get-CimInstance Win32_OperatingSystem).ProductType -ne 1)
+    if (-not $isServer) {
+        return
     }
+
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" # Admin ESC
+    $UserKey  = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" # User ESC
+
+    if (Test-Path $AdminKey) {
+        Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    }
+
+    if (Test-Path $UserKey) {
+        Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    }
+
+    # WARNING: This closes all open File Explorer windows
+    Stop-Process -Name explorer -Force
+
+    # Restart Explorer
+    Start-Process "explorer.exe"
+}
 
 Disable-IEESC
