9f5ca456510cd9c906a3e19f60b7b941a2f98c42
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-06-05 07:22:45 +0100

    Modify BitLocker encryption process

--- includes/ZZ-Enable-BitLocker.ps1
+++ includes/ZZ-Enable-BitLocker.ps1
@@ -4,8 +4,11 @@ $bitlockerStatus = Get-BitLockerVolume -MountPoint 'C:'
 if ($bitlockerStatus.ProtectionStatus -eq 'Off') {
     Write-Host '[INFO] Enabling BitLocker on C: drive...'
 
-    # Enable BitLocker using TPM and create a recovery password
-    Enable-BitLocker -MountPoint 'C:' -EncryptionMethod XtsAes256 -TpmProtector -RecoveryPasswordProtector -UsedSpaceOnly
+    # Enable BitLocker using TPM only and encrypt used space
+    Enable-BitLocker -MountPoint 'C:' -TpmProtector -EncryptionMethod XtsAes256 -UsedSpaceOnly
+
+    # Add a recovery password protector
+    Add-BitLockerKeyProtector -MountPoint 'C:' -RecoveryPasswordProtector | Out-Null
 
     # Retrieve the generated recovery password
     $recoveryPassword = (Get-BitLockerVolume -MountPoint 'C:').KeyProtector |
