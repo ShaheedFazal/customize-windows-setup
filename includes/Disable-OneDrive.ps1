90663e9db3def936e2b55814356eed6633f0530b
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-06-09 14:45:34 +0100

    Delete includes/Disable-OneDrive.ps1

--- includes/Disable-OneDrive.ps1
+++
@@ -1,4 +0,0 @@
-# Disable OneDrive
-Write-Output "Disabling OneDrive..."
-Set-RegistryValue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Value 1 -Type "DWord"
-
