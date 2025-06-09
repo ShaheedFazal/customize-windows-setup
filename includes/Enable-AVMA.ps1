1c7022198446479fb9756a61adf59421e1978268
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-06-09 14:38:53 +0100

    Add server check to AVMA script

--- includes/Enable-AVMA.ps1
+++ includes/Enable-AVMA.ps1
@@ -1,3 +1,10 @@
+# Only run on Windows Server editions
+$isServer = ((Get-CimInstance Win32_OperatingSystem).ProductType -ne 1)
+if (-not $isServer) {
+    Write-Host "Skipping AVMA - Not a Windows Server edition"
+    return
+}
+
 # Set Windows Server 2016 or 2019 Automatic Virtual Machine Activation (AVMA) key
 if ($WINDOWSBUILD -eq $WINDOWSSERVER2016) {
     slmgr /ipk C3RCX-M6NRP-6CXC9-TW2F2-4RHYD
