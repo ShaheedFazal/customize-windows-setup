591c65cd51ef40b4a7aad106d1af2bda39e1a7ca
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-07-09 12:44:09 +0100

    Check existing workgroup before joining

--- includes/Join-Workgroup.ps1
+++ includes/Join-Workgroup.ps1
@@ -1,8 +1,14 @@
-# Automatically join the predefined workgroup
+# Automatically join the predefined workgroup if not already joined
 $CurrentWorkgroup = (Get-WmiObject Win32_ComputerSystem).Workgroup
+$WORKGROUP = 'MYLOCALCHEMIST'
+
 Write-Host ($CR + "Current workgroup: $CurrentWorkgroup") -foregroundcolor $FOREGROUNDCOLOR
 
-$WORKGROUP = 'MYLOCALCHEMIST'
+if ($CurrentWorkgroup -eq $WORKGROUP) {
+    Write-Host ($CR + "Already joined to workgroup '$WORKGROUP'. Skipping.") -foregroundcolor $FOREGROUNDCOLOR $CR
+    return
+}
+
 Write-Host ($CR + "Joining workgroup '$WORKGROUP'") -foregroundcolor $FOREGROUNDCOLOR $CR
 Try {
     Add-Computer -WorkgroupName $WORKGROUP -ErrorAction Stop
