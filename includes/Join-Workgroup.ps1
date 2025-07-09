5c63f4e4bc04d8dc1ca4828b3b4d685cd8ede438
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-07-09 12:40:43 +0100

    Make workgroup join automatic

--- includes/Join-Workgroup.ps1
+++ includes/Join-Workgroup.ps1
@@ -1,17 +1,12 @@
-# Display the current workgroup and ask whether it should be changed
+# Automatically join the predefined workgroup
 $CurrentWorkgroup = (Get-WmiObject Win32_ComputerSystem).Workgroup
 Write-Host ($CR + "Current workgroup: $CurrentWorkgroup") -foregroundcolor $FOREGROUNDCOLOR
-$confirmation = Read-Host "Do you want to change the workgroup? [y/N]"
-if ($confirmation -eq 'y') {
-    $WORKGROUP = Read-Host "Enter new workgroup"
 
-    Write-Host ($CR + "Join to workgroup") -foregroundcolor $FOREGROUNDCOLOR $CR
-    Try {
-        Add-Computer -WorkgroupName $WORKGROUP -ErrorAction Stop
-    } Catch {
-        Write-Warning $Error[0]
-    }
-    Write-Host ("Joined to workgroup $WORKGROUP") -foregroundcolor $FOREGROUNDCOLOR $CR
-} else {
-    Write-Host "Workgroup change skipped." -foregroundcolor $FOREGROUNDCOLOR $CR
+$WORKGROUP = 'MYLOCALCHEMIST'
+Write-Host ($CR + "Joining workgroup '$WORKGROUP'") -foregroundcolor $FOREGROUNDCOLOR $CR
+Try {
+    Add-Computer -WorkgroupName $WORKGROUP -ErrorAction Stop
+} Catch {
+    Write-Warning $Error[0]
 }
+Write-Host ("Joined to workgroup $WORKGROUP") -foregroundcolor $FOREGROUNDCOLOR $CR
