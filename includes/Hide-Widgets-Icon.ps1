ee61d38c73aa36c00dde37e8294da7c9f07badd2
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-07-10 22:32:34 +0100

    Fix Widgets script to use policy keys

--- includes/Hide-Widgets-Icon.ps1
+++ includes/Hide-Widgets-Icon.ps1
@@ -1,2 +1,20 @@
-# Hide Widgets icon on the taskbar
-Set-RegistryValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value 0 -Type "DWord" -Force
+# Hide Widgets icon on the taskbar for all users using policy keys
+Write-Host "Hiding Widgets icon on the taskbar system-wide..."
+
+$policyManagerPath = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\NewsAndInterests\AllowNewsAndInterests"
+$dshPolicyPath     = "HKLM:\SOFTWARE\Policies\Microsoft\Dsh"
+
+try {
+    # Confirm the policy path is writable
+    $regKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SOFTWARE\Microsoft\PolicyManager\default\NewsAndInterests\AllowNewsAndInterests', $true)
+    if (-not $regKey) {
+        throw "Registry path could not be opened with write access"
+    }
+    $regKey.Close()
+
+    Set-RegistryValue -Path $policyManagerPath -Name "value" -Value 0 -Type "DWord" -Force
+    Set-RegistryValue -Path $dshPolicyPath -Name "AllowNewsAndInterests" -Value 0 -Type "DWord" -Force
+    Write-Host "[OK] Policy applied: Widgets icon hidden"
+} catch {
+    Write-Warning "Unable to modify Widgets policy settings. UAC or group policy may restrict write access. Try running PowerShell as Administrator. $_"
+}
