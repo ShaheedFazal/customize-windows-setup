961b72522aaa2143ed7eca00e2b9c664a79244f0
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-08-07 14:46:25 +0100

    Use policy path for Control Panel icon view

--- includes/Set-Control-Panel-View-to-Small-Icons.ps1
+++ includes/Set-Control-Panel-View-to-Small-Icons.ps1
@@ -1,3 +1,9 @@
 # Set Control Panel view to Small icons (Classic)
-Set-RegistryValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "StartupPage" -Value 1 -Type "DWord"
-Set-RegistryValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "AllItemsIconView" -Value 1 -Type "DWord"
+$policyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\ControlPanel"
+
+if (-not (Test-Path $policyPath)) {
+    New-Item -Path $policyPath -Force | Out-Null
+}
+
+Set-RegistryValue -Path $policyPath -Name "StartupPage" -Value 1 -Type "DWord"
+Set-RegistryValue -Path $policyPath -Name "AllItemsIconView" -Value 1 -Type "DWord"
