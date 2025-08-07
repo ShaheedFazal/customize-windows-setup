64d06425f5e376c25e09c4319c474943c88bb5f8
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-08-07 14:46:38 +0100

    Disable autoplay via HKLM policy

--- includes/disabled/Disable-Autoplay.ps1
+++ includes/disabled/Disable-Autoplay.ps1
@@ -1,2 +1,2 @@
-# Disable Autoplay
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Type DWord -Value 1
+# Disable Autoplay globally
+Set-RegistryValue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "NoDriveTypeAutoRun" -Value 255 -Type "DWord" -Force
