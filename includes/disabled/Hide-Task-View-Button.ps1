6afe58ccf60bd2bc2bb4e3812da0d078435e4ed2
ShaheedFazal <shaheed.fazal@mylocalchemist.co.uk>
2025-08-07 14:48:12 +0100

    Hide Task View button via policy

--- includes/disabled/Hide-Task-View-Button.ps1
+++ includes/disabled/Hide-Task-View-Button.ps1
@@ -1,2 +1,8 @@
-# Hide Task View button
-Set-RegistryValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value 0 -Type "DWord" -Force
+# Hide Task View button system-wide via policy
+Write-Host "Hiding Task View button on the taskbar system-wide..."
+
+$policyRoot = "HKLM:\SOFTWARE\Policies\Microsoft\Windows"
+New-Item -Path $policyRoot -Name "Explorer" -Force | Out-Null
+$policyPath = "$policyRoot\Explorer"
+
+Set-RegistryValue -Path $policyPath -Name "ShowTaskViewButton" -Value 0 -Type "DWord" -Force
